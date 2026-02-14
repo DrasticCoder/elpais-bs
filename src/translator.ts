d7f1c23631906a88285bf248526e0bb421f01bf4
Deep Bansode <drasticoder@gmail.com>
2026-02-15 00:16:25 +0530

    chore[translator]: improve performance with parallel

--- src/translator.ts
+++ src/translator.ts
@@ -7,29 +7,19 @@ import axios from 'axios';
 export default async function translator(titles: string[]): Promise<string[]> {
   console.log('translator is called');
 
-  let translatedTitles: string[] = [];
-
-  for (const title of titles) {
-    try {
-      // const encodedTxt = encodeURIComponent(title);
-
-      const resp = await axios.get(
-        'https://ftapi.pythonanywhere.com/translate',
-        {
-          params: {
-            sl: 'es', //source
-            dl: 'en', //destination
-            text: title,
+  return Promise.all(
+    titles.map(async (title) => {
+      try {
+        const resp = await axios.get(
+          'https://ftapi.pythonanywhere.com/translate',
+          {
+            params: { sl: 'es', dl: 'en', text: title },
           },
-        },
-      );
-      const translated = resp.data['destination-text'];
-      translatedTitles.push(translated);
-    } catch (err) {
-      console.error('error translating title:', title, err);
-      translatedTitles.push(title); //falllback
-    }
-  }
-
-  return translatedTitles;
+        );
+        return resp.data['destination-text'];
+      } catch {
+        return title;
+      }
+    }),
+  );
 }
